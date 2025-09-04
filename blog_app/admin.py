a0b02535d74792068a5bc37b070afe27a6a27d60
Thomas Leis <tleis95+github@hotmail.de>
2025-09-04 11:22:26 +0200

    Prepare project for deployment

---
+++ blog_app/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Post, Comment
+
+admin.site.register(User, UserAdmin)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "author", "publish", "status")
+    list_filter = ("status", "created", "publish", "author")
+    search_fields = ("title", "body")
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ("author",)
+    date_hierarchy = "publish"
+    ordering = ("status", "publish")
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
